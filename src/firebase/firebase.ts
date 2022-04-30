75820fc252f87e35c89db0152a8903c0b7309d04
Sebastian Semeniuc <xsebyyy@gmail.com>
2022-04-30 13:23:09 +0300

    little update

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -8,13 +8,7 @@ import { getAuth } from "firebase/auth"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAtAXqs2y8M1RCPVtaak4Xi2rXuCpJ_ATA",
-    authDomain: "expense-tracker-app-c20f7.firebaseapp.com",
-    projectId: "expense-tracker-app-c20f7",
-    storageBucket: "expense-tracker-app-c20f7.appspot.com",
-    messagingSenderId: "906533003432",
-    appId: "1:906533003432:web:1c24be9942dcf699a9a29d",
-    measurementId: "G-LY4BR95HK8",
+    //config
 }
 
 // Initialize Firebase
